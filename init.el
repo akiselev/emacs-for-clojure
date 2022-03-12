aacf06abd9301e65d0fb0b3543b5da529936330a
Alexander Kiselev <alex@akiselev.com>
2022-03-12 13:53:02 -0800

    updated ot lastest cider

--- init.el
+++ init.el
@@ -11,6 +11,11 @@
 (add-to-list 'package-archives
              '("melpa-stable" . "http://stable.melpa.org/packages/") t)
 
+(add-to-list 'package-archives
+             '("melpa-stable" . "http://stable.melpa.org/packages/") t)
+
+(add-to-list 'package-pinned-packages '(cider . "melpa-stable") t)
+
 (add-to-list 'package-pinned-packages '(cider . "melpa-stable") t)
 (add-to-list 'package-pinned-packages '(magit . "melpa-stable") t)
 
